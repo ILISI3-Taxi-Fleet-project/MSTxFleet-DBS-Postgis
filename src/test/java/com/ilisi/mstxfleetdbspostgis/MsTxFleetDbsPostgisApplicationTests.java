dc828b12d48535b3707c114fb2234b9ec7e56013
musdev <w0l1d.dev@gmail.com>
2023-11-09 00:10:55 +0100

    init commit

---
+++ src/test/java/com/ilisi/mstxfleetdbspostgis/MsTxFleetDbsPostgisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetdbspostgis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTxFleetDbsPostgisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
