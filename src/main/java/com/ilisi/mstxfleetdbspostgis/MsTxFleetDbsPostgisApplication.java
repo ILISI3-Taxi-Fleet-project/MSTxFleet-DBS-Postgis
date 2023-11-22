f92a8822d3f90ae0a49d56beb9361f0127dda809
musdev <w0l1d.dev@gmail.com>
2023-11-22 22:35:14 +0100

    #18 replace formatted json as string to UserLocation

--- src/main/java/com/ilisi/mstxfleetdbspostgis/MsTxFleetDbsPostgisApplication.java
+++ src/main/java/com/ilisi/mstxfleetdbspostgis/MsTxFleetDbsPostgisApplication.java
@@ -1,24 +1,13 @@
 package com.ilisi.mstxfleetdbspostgis;
 
-import com.ilisi.mstxfleetdbspostgis.repository.UserLocationRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class MsTxFleetDbsPostgisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsTxFleetDbsPostgisApplication.class, args);
-    }
-//    @Bean
-    CommandLineRunner commandLineRunner(UserLocationRepository userLocationRepository) {
-        return args -> {
-
-            log.info(userLocationRepository.findNearbyOnlineUsers("POINT(-7.5790 33.5521)", 1000).toString());
-        };
     }
 }
 
